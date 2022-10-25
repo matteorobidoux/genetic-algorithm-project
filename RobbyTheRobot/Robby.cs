375243249ea7c09cc1710644d56ab8c522d9767e
vlad.ik86 <vlad.gavrilkov@gmail.com>
2022-10-25 13:12:34 -0400

    Initial project commit

---
+++ RobbyTheRobot/Robby.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace RobbyTheRobot
+{
+    public static class Robby
+    {
+        public static IRobbyTheRobot CreateRobby()
+        {
+            return null;
+        }
+    }
+}
