f4d67d40d63362e063d08d69869b165d451efbc1
Matteo Robidoux <SDJ00008@gmail.com>
2022-10-27 21:34:01 -0400

    Created Grid using sprite and Cookie Monster Sprite, classes may change depending on instructions but general idea is there

--- RobbyVisualizer/RobbyVisualizerGame.cs
+++ RobbyVisualizer/RobbyVisualizerGame.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace RobbyVisualizer
 {
@@ -8,6 +9,8 @@ namespace RobbyVisualizer
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private SimulationSprite[,] _grid;
+        private CookieMonsterSprite _cookieMonster;
 
         public RobbyVisualizerGame()
         {
@@ -18,16 +21,45 @@ namespace RobbyVisualizer
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            _graphics.PreferredBackBufferWidth = 1770;
+            _graphics.PreferredBackBufferHeight = 980;
+            _graphics.ApplyChanges();
 
+            _grid = new SimulationSprite[10,10];
+
+            int xPos = 470;
+            int yPos = 30;
+
+            for(int i = 0; i< 10; i++){
+                for(int j = 0; j< 10; j++){
+                    Color color;
+                    if((i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 == 1)){
+                        color = Color.DarkRed;
+                    } else {
+                        color = Color.White;
+                    }
+
+                    SimulationSprite gridUnit = new SimulationSprite(this, xPos, yPos, color);
+                    _grid[i, j] = gridUnit;
+                    Components.Add(gridUnit);
+                    xPos += 78;
+                }
+                xPos = 470;
+                yPos += 78;
+            }
+
+            // Moving square to square is -10 x and -10 y
+            _cookieMonster = new CookieMonsterSprite(this, 200, 20);
+            Components.Add(_cookieMonster);
+            // TODO: Add your initialization logic here
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
             // TODO: use this.Content to load your game content here
+            base.LoadContent();
         }
 
         protected override void Update(GameTime gameTime)
@@ -43,7 +75,6 @@ namespace RobbyVisualizer
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
---
+++ RobbyVisualizer/SimulationSprite.cs
@@ -0,0 +1,53 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace RobbyVisualizer
+{
+    public class SimulationSprite : DrawableGameComponent
+    {
+        private Game _game;
+        private SpriteBatch _spriteBatch;
+        private Texture2D _squareSprite;
+        private int _xPosition;
+        private int _yPosition;
+        private Color _color;
+
+        public SimulationSprite(Game game, int xPosition, int yPosition, Color color) : base(game){
+            _game = game;
+            _xPosition = xPosition;
+            _yPosition = yPosition;
+            _color = color;
+        }
+
+        public override void Initialize()
+        {
+            // Calls base Initialize
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // Created keySprite with the keyType (Image name) as the image
+            _squareSprite = _game.Content.Load<Texture2D>("Square");
+
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            // Calls base Update
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_squareSprite, new Rectangle(_xPosition,_yPosition,78,78), _color);
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
