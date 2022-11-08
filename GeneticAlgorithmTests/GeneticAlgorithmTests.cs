a4e4f76c7166f05a1a0bd10fddabcd28999376d0
Nolan Ganz <nolanganz1@gmail.com>
2022-11-08 10:50:13 -0500

    Tested generating multiple generations with an even population size

--- GeneticAlgorithmTests/GeneticAlgorithmTests.cs
+++ GeneticAlgorithmTests/GeneticAlgorithmTests.cs
@@ -51,6 +51,45 @@ namespace GeneticAlgorithmTests
       }
     }
 
+    [TestMethod]
+    public void TestGenerateGenerationMultiple()
+    {
+      var alg = new GeneticAlgorithm.GeneticAlgorithm(10, 10, 5, .5, .5, 5, MockCalcFitness, 0);
+      alg.GenerateGeneration();
+      alg.GenerateGeneration();
+      Assert.AreEqual(2, alg.GenerationCount);
+      Generation g1 = alg.CurrentGeneration as Generation;
+      int[] expectedGenes = new int[] {3, 4, 3, 2, 1, 2, 4, 2, 4, 1};
+      int[] expectedKid1 = new int[] { 3, 4, 2, 4, 3, 4, 4, 2, 3, 1 };
+      int[] expectedKid2 = new int[] { 3, 4, 3, 1, 4, 2, 4, 2, 4, 1 };
+      Assert.AreEqual(10, g1.NumberOfChromosomes);
+      for (int i = 0; i < g1.NumberOfChromosomes; i++)
+      {
+        Assert.AreEqual(10, g1[i].Length);
+        if (i < 4)
+        {
+          for (int j = 0; j < g1[i].Length; j++)
+          {
+            Assert.AreEqual(expectedGenes[j], g1[i][j]);
+          }
+        }
+        else if (i % 2 == 0)
+        {
+          for (int j = 0; j < g1[i].Length; j++)
+          {
+            Assert.AreEqual(expectedKid1[j], g1[i][j]);
+          }
+        }
+        else
+        {
+          for (int j = 0; j < g1[i].Length; j++)
+          {
+            Assert.AreEqual(expectedKid2[j], g1[i][j]);
+          }
+        }
+      }
+    }
+
     private double MockCalcFitness(IChromosome chromosome, IGeneration generation)
     {
       return chromosome[0];
