31118a10334c019e6f809e5f9c0c5078193fb6cc
Nolan Ganz <nolanganz1@gmail.com>
2022-11-08 14:34:44 -0500

    Added random tests

--- GeneticAlgorithmTests/GeneticAlgorithmTests.cs
+++ GeneticAlgorithmTests/GeneticAlgorithmTests.cs
@@ -34,6 +34,21 @@ namespace GeneticAlgorithmTests
     }
 
     [TestMethod]
+    public void TestRandomCtor()
+    {
+      var alg = new GeneticAlgorithm.GeneticAlgorithm(10, 10, 7, .5, .5, 10, MockCalcFitness);
+      Assert.AreEqual(10, alg.PopulationSize);
+      Assert.AreEqual(10, alg.NumberOfGenes);
+      Assert.AreEqual(7, alg.LengthOfGene);
+      Assert.AreEqual(0.5, alg.MutationRate);
+      Assert.AreEqual(0.5, alg.EliteRate);
+      Assert.AreEqual(10, alg.NumberOfTrials);
+      Assert.AreEqual(MockCalcFitness, alg.FitnessCalculation);
+      Assert.AreEqual(0, alg.GenerationCount);
+      Assert.IsNull(alg.CurrentGeneration);
+    }
+
+    [TestMethod]
     public void TestGenerateGeneration()
     {
       var alg = new GeneticAlgorithm.GeneticAlgorithm(10, 10, 5, .5, .5, 5, MockCalcFitness, 0);
@@ -42,6 +57,7 @@ namespace GeneticAlgorithmTests
       Generation g1 = alg.CurrentGeneration as Generation;
       int[] expectedGenes = new int[] {3, 4, 3, 2, 1, 2, 4, 2, 4, 1};
       Assert.AreEqual(10, g1.NumberOfChromosomes);
+      Assert.AreEqual(3, g1.MaxFitness, 0.0000001);
       for (int i = 0; i < g1.NumberOfChromosomes; i++)
       {
         Assert.AreEqual(10, g1[i].Length);
@@ -49,6 +65,27 @@ namespace GeneticAlgorithmTests
         {
           Assert.AreEqual(expectedGenes[j], g1[i][j]);
         }
+        Assert.AreEqual(3, g1[i].Fitness, 0.0000001);
+      }
+    }
+
+    [TestMethod]
+    public void TestRandomGenerateGeneration()
+    {
+      var alg = new GeneticAlgorithm.GeneticAlgorithm(10, 10, 5, .5, .5, 5, MockCalcFitness);
+      alg.GenerateGeneration();
+      Assert.AreEqual(1, alg.GenerationCount);
+      Generation g1 = alg.CurrentGeneration as Generation;
+      Assert.AreEqual(10, g1.NumberOfChromosomes);
+      Assert.AreEqual(2, g1.MaxFitness, 2.0000001);
+      for (int i = 0; i < g1.NumberOfChromosomes; i++)
+      {
+        Assert.AreEqual(10, g1[i].Length);
+        for (int j = 0; j < g1[i].Length; j++)
+        {
+          Assert.AreEqual(2, g1[i][j], 2);
+        }
+        Assert.AreEqual(2, g1[i].Fitness, 2.0000001);
       }
     }
 
@@ -64,6 +101,7 @@ namespace GeneticAlgorithmTests
       int[] expectedKid1 = new int[] { 3, 4, 2, 4, 3, 4, 4, 2, 3, 1 };
       int[] expectedKid2 = new int[] { 3, 4, 3, 1, 4, 2, 4, 2, 4, 1 };
       Assert.AreEqual(10, g1.NumberOfChromosomes);
+      Assert.AreEqual(3, g1.MaxFitness, 0.0000001);
       for (int i = 0; i < g1.NumberOfChromosomes; i++)
       {
         Assert.AreEqual(10, g1[i].Length);
@@ -88,6 +126,28 @@ namespace GeneticAlgorithmTests
             Assert.AreEqual(expectedKid2[j], g1[i][j]);
           }
         }
+        Assert.AreEqual(3, g1[i].Fitness, 0.0000001);
+      }
+    }
+
+    [TestMethod]
+    public void TestRandomGenerateGenerationMultipleEvenPop()
+    {
+      var alg = new GeneticAlgorithm.GeneticAlgorithm(10, 10, 5, .5, .5, 5, MockCalcFitness);
+      alg.GenerateGeneration();
+      alg.GenerateGeneration();
+      Assert.AreEqual(2, alg.GenerationCount);
+      Generation g1 = alg.CurrentGeneration as Generation;
+      Assert.AreEqual(10, g1.NumberOfChromosomes);
+      Assert.AreEqual(2, g1.MaxFitness, 2.0000001);
+      for (int i = 0; i < g1.NumberOfChromosomes; i++)
+      {
+        Assert.AreEqual(10, g1[i].Length);
+        for (int j = 0; j < g1[i].Length; j++)
+        {
+          Assert.AreEqual(2, g1[i][j], 2);
+        }
+        Assert.AreEqual(2, g1[i].Fitness, 2.0000001);
       }
     }
 
@@ -103,6 +163,7 @@ namespace GeneticAlgorithmTests
       int[] expectedKid1 = new int[] { 3, 4, 2, 4, 3, 4, 4, 2, 3, 1 };
       int[] expectedKid2 = new int[] { 3, 4, 3, 1, 4, 2, 4, 2, 4, 1 };
       Assert.AreEqual(11, g1.NumberOfChromosomes);
+      Assert.AreEqual(3, g1.MaxFitness, 0.0000001);
       for (int i = 0; i < g1.NumberOfChromosomes; i++)
       {
         Assert.AreEqual(10, g1[i].Length);
@@ -127,6 +188,28 @@ namespace GeneticAlgorithmTests
             Assert.AreEqual(expectedKid2[j], g1[i][j]);
           }
         }
+        Assert.AreEqual(3, g1[i].Fitness, 0.0000001);
+      }
+    }
+
+    [TestMethod]
+    public void TestRandomGenerateGenerationMultipleOddPop()
+    {
+      var alg = new GeneticAlgorithm.GeneticAlgorithm(11, 10, 5, .5, .5, 5, MockCalcFitness);
+      alg.GenerateGeneration();
+      alg.GenerateGeneration();
+      Assert.AreEqual(2, alg.GenerationCount);
+      Generation g1 = alg.CurrentGeneration as Generation;
+      Assert.AreEqual(11, g1.NumberOfChromosomes);
+      Assert.AreEqual(2, g1.MaxFitness, 2.0000001);
+      for (int i = 0; i < g1.NumberOfChromosomes; i++)
+      {
+        Assert.AreEqual(10, g1[i].Length);
+        for (int j = 0; j < g1[i].Length; j++)
+        {
+          Assert.AreEqual(2, g1[i][j], 2);
+        }
+        Assert.AreEqual(2, g1[i].Fitness, 2.0000001);
       }
     }
 
