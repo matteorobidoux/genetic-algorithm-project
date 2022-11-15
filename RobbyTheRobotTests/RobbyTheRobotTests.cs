a1d92c5a6826822b187b1421fffcf804e6aa7465
Nolan Ganz <nolanganz1@gmail.com>
2022-11-15 12:04:08 -0500

    Changed tests to account for new info

--- RobbyTheRobotTests/RobbyTheRobotTests.cs
+++ RobbyTheRobotTests/RobbyTheRobotTests.cs
@@ -2,6 +2,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RobbyTheRobot;
 using System;
 using System.IO;
+using System.Threading;
 
 namespace RobbyTheRobotTests
 {
@@ -81,19 +82,19 @@ namespace RobbyTheRobotTests
             var robby = Robby.CreateRobby(200, 1, 10, 10, 0.5, 0.5, 200, seed);
 
             //automatically create test directory
-            string testOutputDirectory = "../../../GenerationsBinSeededTests";
+            string testOutputDirectory = "../../../TEMP/GenerationsBinSeededTests";
             Directory.CreateDirectory(testOutputDirectory);
 
             //write file in ./TestRobbyTheRobot/GenerationBinTests/
             robby.GeneratePossibleSolutions(testOutputDirectory);
 
-            string expectedOutputForGen1 = "0,200,1,0,3,5,4,3,2,6,0,4,0,1,2,6,4,4,1,4,4,4,6,0,1,2,5,1,5,2,5,6,3,5,4,0,6,5,6,0,3,3,1,6,4,2,3,4,4,3,1,2,3,1,2,4,2,1,6,0,6,3,4,0,0,1,0,2,3,2,6,6,5,1,0,1,3,6,2,5,6,0,3,2,0,3,2,2,2,0,4,5,5,5,0,0,6,1,6,3,2,0,0,2,3,2,3,2,0,1,1,3,5,1,0,5,6,2,6,4,5,4,6,3,4,1,0,0,0,0,2,1,0,6,3,5,6,0,5,4,6,0,2,1,5,1,0,4,4,1,0,3,1,1,4,4,1,4,4,2,2,3,1,6,6,0,2,5,4,2,5,6,3,2,5,5,2,5,5,3,0,2,2,6,0,5,6,6,2,6,4,2,3,3,2,2,6,2,2,4,5,1,6,2,2,1,0,6,3,5,1,3,5,1,1,0,1,1,6,1,0,3,3,5,4,3,3,6,6,2,6,0,2,0,4,5,2,0,3,6,4,0,2,3,4";
+            string expectedOutputForGen1 = "1,-970,200,1,0,3,5,4,3,2,6,0,4,0,1,2,6,4,4,1,4,4,4,6,0,1,2,5,1,5,2,5,6,3,5,4,0,6,5,6,0,3,3,1,6,4,2,3,4,4,3,1,2,3,1,2,4,2,1,6,0,6,3,4,0,0,1,0,2,3,2,6,6,5,1,0,1,3,6,2,5,6,0,3,2,0,3,2,2,2,0,4,5,5,5,0,0,6,1,6,3,2,0,0,2,3,2,3,2,0,1,1,3,5,1,0,5,6,2,6,4,5,4,6,3,4,1,0,0,0,0,2,1,0,6,3,5,6,0,5,4,6,0,2,1,5,1,0,4,4,1,0,3,1,1,4,4,1,4,4,2,2,3,1,6,6,0,2,5,4,2,5,6,3,2,5,5,2,5,5,3,0,2,2,6,0,5,6,6,2,6,4,2,3,3,2,2,6,2,2,4,5,1,6,2,2,1,0,6,3,5,1,3,5,1,1,0,1,1,6,1,0,3,3,5,4,3,3,6,6,2,6,0,2,0,4,5,2,0,3,6,4,0,2,3,4";
             string expectedOutputForAllGens = "10,200,0,0,3,1,4,4,2,1,5,4,0,6,2,6,4,4,1,2,1,6,0,3,1,0,5,1,0,2,5,6,3,2,3,0,5,5,6,0,3,3,0,0,5,5,3,5,4,0,1,2,3,1,2,4,5,2,6,0,2,5,4,0,0,1,0,6,0,5,6,1,0,1,3,1,2,6,2,5,4,2,3,2,4,3,6,2,2,3,1,4,2,3,2,3,6,1,6,5,1,0,0,6,5,3,6,2,6,1,2,1,3,1,0,5,6,2,2,4,5,0,6,3,0,4,5,0,0,0,6,1,0,4,3,5,6,0,5,4,1,0,1,1,2,1,0,4,4,4,2,3,1,1,4,4,1,1,4,6,2,2,1,6,6,4,2,5,3,2,5,2,3,6,4,5,2,4,5,3,0,4,5,6,4,5,6,6,2,4,1,2,6,3,0,2,3,2,2,0,5,1,3,3,3,4,3,6,4,4,1,3,2,0,1,2,1,1,4,1,0,6,3,3,0,0,6,3,1,2,4,0,2,0,4,5,2,0,3,6,4,6,2,3,5";
 
             Assert.AreEqual(expectedOutputForGen1, File.ReadAllText($"{testOutputDirectory}/generation1.txt"));
-            Assert.AreEqual(expectedOutputForAllGens, File.ReadAllText($"{testOutputDirectory}/generation2.txt"));
-            Assert.AreEqual(expectedOutputForAllGens, File.ReadAllText($"{testOutputDirectory}/generation5.txt"));
-            Assert.AreEqual(expectedOutputForAllGens, File.ReadAllText($"{testOutputDirectory}/generation10.txt"));
+            Assert.AreEqual("2,"+expectedOutputForAllGens, File.ReadAllText($"{testOutputDirectory}/generation2.txt"));
+            Assert.AreEqual("5,"+expectedOutputForAllGens, File.ReadAllText($"{testOutputDirectory}/generation5.txt"));
+            Assert.AreEqual("10,"+expectedOutputForAllGens, File.ReadAllText($"{testOutputDirectory}/generation10.txt"));
         }
 
         //Expects the event to be raised 6 times for 100 generations. (not seeded)
@@ -109,11 +110,12 @@ namespace RobbyTheRobotTests
             };
 
             //automatically create test directory
-            string testOutputDirectory = "../../../GenerationsBinEventTests";
+            string testOutputDirectory = "../../../TEMP/GenerationsBinEventTests";
             Directory.CreateDirectory(testOutputDirectory);
 
             //write file in ./TestRobbyTheRobot/GenerationBinTests/
             robby.GeneratePossibleSolutions(testOutputDirectory);
+            Thread.Sleep(500);
 
             Assert.AreEqual(6, eventCallCounter);
         }
