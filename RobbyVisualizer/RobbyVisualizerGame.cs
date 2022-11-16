81f88f02abb64fe2c72b7a5dc47eba3bf90384aa
Matteo Robidoux <SDJ00008@gmail.com>
2022-11-15 23:24:17 -0500

    Testing with scoreAllele and need to pull from main to retrieve working version of getRandomGrid

--- RobbyVisualizer/RobbyVisualizerGame.cs
+++ RobbyVisualizer/RobbyVisualizerGame.cs
@@ -7,6 +7,7 @@ using System.Windows.Forms;
 using System.Threading;
 using System.Threading.Tasks;
 using System;
+using RobbyTheRobot;
 
 namespace RobbyVisualizer
 {
@@ -22,8 +23,14 @@ namespace RobbyVisualizer
         private SpriteFont _infoFontSprite;
         private Texture2D _background;
         private bool _run;
-        private string[] _moves;
+        private string[] _fileDetails;
         private int _numOfMoves;
+        private IRobbyTheRobot _robby;
+        private ContentsOfGrid[,] _contentGrid;
+        private int _baseX;
+        private int _baseY;
+        private int _sizeIncrement;
+        private bool _displayNewGrid;
 
         public RobbyVisualizerGame()
         {
@@ -31,8 +38,16 @@ namespace RobbyVisualizer
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
             _run = false;
-            _moves = null;
+            _fileDetails = null;
             _numOfMoves = 0;
+            _robby = Robby.CreateRobby(200, 1, 10, 100, 0.5, 0.5, 200, null);
+            _contentGrid =  _robby.GenerateRandomTestGrid();
+            _baseX = 470;
+            _baseY = 30;
+            _sizeIncrement = 78;
+            _displayNewGrid = true;
+
+
         }
 
         protected override void Initialize()
@@ -44,8 +59,8 @@ namespace RobbyVisualizer
             _grid = new GridUnitSprite[10,10];
             _cookies = new CookieSprite[10,10];
 
-            int xPos = 470;
-            int yPos = 30;
+            int xPos = _baseX;
+            int yPos = _baseY;
 
             for(int i = 0; i< 10; i++){
                 for(int j = 0; j< 10; j++){
@@ -60,20 +75,11 @@ namespace RobbyVisualizer
                     _grid[i, j] = gridUnit;
                     Components.Add(gridUnit);
 
-                    if((i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 == 1)){
-                        CookieSprite cookie = new CookieSprite(this, xPos-10, yPos-10);
-                        _cookies[i, j] = cookie;
-                        Components.Add(cookie);
-                    }
-                    xPos += 78;
+                    xPos += _sizeIncrement;
                 }
-                xPos = 470;
-                yPos += 78;
+                xPos = _baseX;
+                yPos += _sizeIncrement;
             }           
-
-            // Moving square to square is -10 x and -10 y
-            _cookieMonster = new CookieMonsterSprite(this, 460, 20);
-            Components.Add(_cookieMonster);
 
             _buttonSprite = new ButtonSprite(this, 650, 930);
             Components.Add(_buttonSprite);
@@ -97,6 +103,22 @@ namespace RobbyVisualizer
 
         protected override void Update(GameTime gameTime)
         {
+            if(_displayNewGrid){
+                for(int i = 0; i< 10; i++){
+                    for(int j = 0; j< 10; j++){
+                        if(_contentGrid[i,j] == ContentsOfGrid.Can){
+                            CookieSprite cookie = new CookieSprite(this, _grid[i,j].XPosition-10, _grid[i,j].YPosition-10);
+                            _cookies[i, j] = cookie;
+                            Components.Add(cookie);
+                        }
+                    }
+                }
+                // Moving square to square is -10 x and -10 y
+                _cookieMonster = new CookieMonsterSprite(this, _baseX - 10, _baseY - 10);
+                Components.Add(_cookieMonster);
+                _displayNewGrid = false;
+            }
+
             OnCookie();
 
             if(_buttonSprite.IsClicked){
@@ -115,10 +137,10 @@ namespace RobbyVisualizer
 
             _spriteBatch.Begin();
             _spriteBatch.Draw(_background, new Rectangle(0,0,1950, 1125),Color.White);
-            if(_moves != null){
-                _spriteBatch.DrawString(_infoFontSprite, $"Generation: {_moves[0]}", new Vector2(470, 820), Color.White);
-                _spriteBatch.DrawString(_infoFontSprite, $"Move: {_numOfMoves}/{_moves[2]}", new Vector2(470, 855), Color.White);
-                _spriteBatch.DrawString(_infoFontSprite, $"Points: /{_moves[1]}", new Vector2(470, 890), Color.White);
+            if(_fileDetails != null){
+                _spriteBatch.DrawString(_infoFontSprite, $"Generation: {_fileDetails[0]}", new Vector2(470, 820), Color.White);
+                _spriteBatch.DrawString(_infoFontSprite, $"Move: {_numOfMoves}/{_fileDetails[2]}", new Vector2(470, 855), Color.White);
+                _spriteBatch.DrawString(_infoFontSprite, $"Points: /{_fileDetails[1]}", new Vector2(470, 890), Color.White);
             }
             _spriteBatch.End();
             // TODO: Add your drawing code here
@@ -128,21 +150,31 @@ namespace RobbyVisualizer
         public void MoveCookieMonster(){
             var task = new Task(() => {
                 foreach(string file in _buttonSprite.Files){
-                    _moves = System.IO.File.ReadAllText(file).Split(",");
-                    for(int i = 3; i < Int32.Parse(_moves[2])+3; i++){
-                        if(_moves[i] == "0" && _cookieMonster.YPosition - 78 >= 20){
-                            _cookieMonster.YPosition -= 78;
-                        } else if(_moves[i] == "1" && _cookieMonster.YPosition + 78 <= 722){
-                        _cookieMonster.YPosition += 78;
-                        } else if(_moves[i] == "2" && _cookieMonster.XPosition + 78 <= 1162){
-                            _cookieMonster.XPosition += 78;
-                        } else if(_moves[i] == "3" && _cookieMonster.XPosition - 78 >= 460){
-                            _cookieMonster.XPosition -= 78;
-                        } else if(_moves[i] == "5"){
-                            _cookieMonster.Eating = true;
-                        } else if(_moves[i] == "6"){
+                    _fileDetails = System.IO.File.ReadAllText(file).Split(",");
+                    int[] moves = new int[_fileDetails.Length-3];
+                    for(int i = 3; i < _fileDetails.Length; i++){
+                        moves[i-3] = Convert.ToInt32(_fileDetails[i]);
+                    }
+                    int xPosition = 1;
+                    int yPosition = 1;
+
+                    for(int i = 3; i < Int32.Parse(_fileDetails[2])+3; i++){
+                        RobbyHelper.ScoreForAllele(moves,_contentGrid, new Random(),ref xPosition, ref yPosition);
+                        _cookieMonster.XPosition = xPosition * _sizeIncrement;
+                        _cookieMonster.YPosition = yPosition * _sizeIncrement;
+                        // if(_moves[i] == "0" && _cookieMonster.YPosition - 78 >= 20){
+                        //     _cookieMonster.YPosition -= 78;
+                        // } else if(_moves[i] == "1" && _cookieMonster.YPosition + 78 <= 722){
+                        // _cookieMonster.YPosition += 78;
+                        // } else if(_moves[i] == "2" && _cookieMonster.XPosition + 78 <= 1162){
+                        //     _cookieMonster.XPosition += 78;
+                        // } else if(_moves[i] == "3" && _cookieMonster.XPosition - 78 >= 460){
+                        //     _cookieMonster.XPosition -= 78;
+                        // } else if(_moves[i] == "5"){
+                        //     _cookieMonster.Eating = true;
+                        // } else if(_moves[i] == "6"){
                             
-                        } 
+                        // } 
                         Thread.Sleep(500);
                         _numOfMoves++;
                     }
